dcbe47052e2b437f11aa3a781cd5204277492aa0
Andy Pavlo <pavlo@cs.brown.edu>
2016-02-07 22:00:28 -0500

    Checkpoint from claudius

--- common/basetest.py
+++ common/basetest.py
@@ -1,35 +1,40 @@
 
+import os
+import sys
 import psycopg2
 import unittest
 import logging
 
+from ConfigParser import RawConfigParser
+
 LOG = logging.getLogger(__name__)
 
 class BaseTest(unittest.TestCase):
+    """Base test case code that will connect to the oracle and target database"""
     
-    def __init__(self, config):
+    def __init__(self, configPath):
         
-        setupVars = {
-            "oracle": [ ],
-            "target": [ ],
-        }
+        # Load in configuration file
+        self.config = RawConfigParser()
+        self.config.read(configPath)
         
-        for db in setupVars.keys():
-            conn = None
+        # Initialize database connections
+        for db in ["oracle", "target"]:
             try:
-                conn = psycopg2.connect(database=config.get(db, ['db_name']), 
-                                        user=config.get(db, ['db_user']),
-                                        password=config.get(db, ['db_pass']),
-                                        host=config.get(db, ['db_host']),
-                                        port=config.get(db, ['db_port']))
+                self.__dict__[db] = psycopg2.connect(
+                    host=self.config.get(db, 'db_host'),
+                    port=self.config.get(db, 'db_port'),
+                    database=self.config.get(db, 'db_name'), 
+                    user=self.config.get(db, 'db_user'),
+                    password=self.config.get(db, 'db_pass'),
+                )
             except:
                 LOG.error("Unable to connect to %s database" % db.upper())
                 raise
-            assert(not conn is None)
-            self.__dict__[db] = conn
+            assert(not self.__dict__[db] is None)
+            LOG.debug("Connected to %s database" % db.upper())
         ## FOR
     ## DEF
-
     
     def setUp(self):
         pass
@@ -38,6 +43,6 @@ class BaseTest(unittest.TestCase):
 ## CLASS
 
 if __name__ == '__main__':
-    print "HELLO"
+    BaseTest(os.path.realpath("../test.conf"))
 
 ## IF
