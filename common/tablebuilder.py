587fd43d0781a26aca8ff3c46e8c3ed5fe33cbf7
Andy Pavlo <pavlo@cs.brown.edu>
2016-03-12 21:45:33 -0500

    Working on SQLAlchemy integration with dyanmic table builder

--- common/tablebuilder.py
+++ common/tablebuilder.py
@@ -4,39 +4,48 @@ import os
 import sys
 import logging
 
+import sqlalchemy 
+
 LOG = logging.getLogger(__name__)
 
 # ==================================================================
 
 SQL_TYPES = {
-    "STRING": [
-        "CHAR",
-        "VARCHAR",
-        "TEXT",
-        "VARBINARY",
-    ],
-    "NUMERIC": [
-        "TINYINT",
-        "SMALLINT",
-        "INT",
-        "BIGINT",
-    ],
-    "DECIMAL": [
-        "DECIMAL",
-        "NUMERIC",
-        "REAL",
-    ],
-    "TIMESTAMP": [
-        "TIMESTAMP",
-        "DATE",
-    ],
+    "STRING": {
+        "CHAR": sqlalchemy.types.String,
+        "VARCHAR": sqlalchemy.types.String,
+        "TEXT": sqlalchemy.types.Text,
+        "VARBINARY": None,
+    },
+    "NUMERIC": {
+        "TINYINT": None,
+        "SMALLINT": None,
+        "MEDIUMINT": None,
+        "INT": sqlalchemy.types.Integer,
+        "BIGINT": sqlalchemy.types.BigInteger,
+    },
+    "DECIMAL": {
+        "DECIMAL": None,
+        "NUMERIC": sqlalchemy.types.Numeric,
+        "REAL": None,
+        "FLOAT": sqlalchemy.types.Float,
+    },
+    "TIMESTAMP": {
+        "TIMESTAMP": None,
+        "TIME": sqlalchemy.types.Time,
+        "DATE": sqlalchemy.types.Date,
+        "DATETIME": sqlalchemy.types.DateTime,
+    },
+    "MISC": {
+        "BOOLEAN": sqlalchemy.types.Boolean,
+    }
 }
-ALL_TYPES = [ ]
+ALL_TYPES = { }
 for category in SQL_TYPES:
-    for x in SQL_TYPES[category]:
+    for x,y in SQL_TYPES[category].items():
         varName = "TYPE_%s" % x
-        globals()[varName] = SQL_TYPES[category]
-        ALL_TYPES.append(SQL_TYPES[category])
+        globals()[varName] = x
+        ALL_TYPES[x] = y # SQL_TYPES[category])
 ## FOR
 
 # ==================================================================
@@ -44,9 +53,11 @@ for category in SQL_TYPES:
 
 class TableBuilder():
     
-    def __init__(self, tableName):
+    def __init__(self, engine, tableName):
+        self.engine = engine
+        self.metadata = sqlalchemy.MetaData(bind=self.engine)
         self.tableName = tableName
-        self.attributes = [ ]
+        self.table = sqlalchemy.Table(self.tableName, self.metadata)
         self.attributeCtr = 0
     ## DEF
     
@@ -55,24 +66,29 @@ class TableBuilder():
         return "attr_%02d" % self.attributeCtr
     ## DEF
     
-    def addIntColumn(self, unique=False):
-        attr = Attribute(self.__nextName(), TYPE_INT, attrUnique=unique)
-        self.attributes.append(attr)
+    def addAttribute(self, attrType, primaryKey=False, attrLength=None, attrNull=True, attrUnique=False):
+        if not attrType in ALL_TYPES:
+            raise Exception("Unknown type '%s'" % attrType)
+        if ALL_TYPES[attrType] is None:
+            raise Exception("Unsupported type '%s'" % attrType)
+        
+        attrName = self.__nextName()
+        targetAttrType = "sqlalchemy.sql.sqltypes.%s" % ALL_TYPES[attrType]
+        if attrLength != None:
+            targetAttrType += "(%d)" % attrLength
+        eval("attrType = " + targetAttrType)
+
+        attr = sqlalchemy.Column(attrName, attrType, 
+                                 primary_key=primaryKey,
+                                 nullable=attrNull,
+                                 unique=attrUnique)
     ## DEF
         
-    
-    
-    
-    
-## CLASS
-
-class Attribute():
-    
-    def __init__(self, attrName, attrType, attrLength=None, attrUnique=False):
-        assert(attrType in ALL_TYPES)
-        self.attrName = attrName
-        self.attrType = attrType
+    def create():
+        assert not self.table is None
+        self.table.create()
     ## DEF
+    
     
     
 ## CLASS
