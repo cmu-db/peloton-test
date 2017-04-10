edf52bf60098694b1d4c79ab360fc13f90188d62
Tianyu Li <tli2@andrew.cmu.edu>
2017-04-10 11:21:10 -0400

    Generate XML file for test output

--- src/main/java/edu/cmu/cs/db/peloton/test/common/Main.java
+++ src/main/java/edu/cmu/cs/db/peloton/test/common/Main.java
@@ -1,12 +1,22 @@
 package edu.cmu.cs.db.peloton.test.common;
 
 import edu.cmu.cs.db.peloton.test.generate.ast.Ast;
-import edu.cmu.cs.db.peloton.test.generate.ast.Context;
 import edu.cmu.cs.db.peloton.test.generate.defn.stochastic.Select;
+import junit.framework.Test;
+import junit.framework.TestResult;
+import org.apache.tools.ant.taskdefs.optional.junit.JUnitResultFormatter;
+import org.apache.tools.ant.taskdefs.optional.junit.JUnitTest;
+import org.apache.tools.ant.taskdefs.optional.junit.XMLJUnitResultFormatter;
+import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.sql.*;
 import java.util.Random;
 
@@ -14,9 +24,10 @@ import java.util.Random;
  * Created by tianyuli on 3/20/17.
  */
 public class Main {
-    public static DatabaseWrapper db;
-    public static Ast.StochasticElem tested;
-    public static Random random;
+    static DatabaseWrapper testDb;
+    static DatabaseWrapper oracleDb;
+    static Ast.StochasticElem tested;
+    static Random random;
     /**
      * The entry point of application.
      *
@@ -24,7 +35,15 @@ public class Main {
      * @throws SQLException the sql exception
      */
     public static void main(String[] args) throws SQLException {
-        db = new DatabaseWrapper(null, args[0], Integer.parseInt(args[1]), args[2]){
+        testDb = new DatabaseWrapper(null, args[0], Integer.parseInt(args[1]), args[2]){
+            @Override
+            protected Connection initiateConnection(String hostname, int port, String dbName) throws SQLException {
+                return DriverManager.getConnection(
+                        String.format("jdbc:postgresql://%s:%d/%s", hostname, port, dbName));
+            }
+        };
+
+        oracleDb = new DatabaseWrapper(null, args[3], Integer.parseInt(args[4]), args[5]) {
             @Override
             protected Connection initiateConnection(String hostname, int port, String dbName) throws SQLException {
                 return DriverManager.getConnection(
@@ -34,10 +53,136 @@ public class Main {
         tested = new Select();
         random = new Random();
 
-        Result result = JUnitCore.runClasses(TestQuery.class);
-        for (Failure f : result.getFailures()) {
-            System.out.println(f.toString());
-        }
-        System.out.println(result.wasSuccessful());
+        /* Adopted from https://ttddyy.github.io/generate-junit-xml-report-from-junitcore/ */
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new JUnitResultFormatterAsRunListener(new XMLJUnitResultFormatter()) {
+            @Override
+            public void testStarted(Description description) throws Exception {
+                formatter.setOutput(new FileOutputStream(
+                        new File("test_reports/","TEST-" + description.getDisplayName() + ".xml")));
+                super.testStarted(description);
+            }
+        });
+        junit.run(TestQuery.class);
     }
+
+    /* Adopted from https://github.com/cloudbees/junit-standalone-runner/ */
+
+    /**
+     * Wraps {@link Description} into {@link Test} enough to fake {@link JUnitResultFormatter}.
+     */
+    public static class DescriptionAsTest implements Test {
+        private final Description description;
+
+        public DescriptionAsTest(Description description) {
+            this.description = description;
+        }
+
+        public int countTestCases() {
+            return 1;
+        }
+
+        public void run(TestResult result) {
+            throw new UnsupportedOperationException();
+        }
+
+        /**
+         * {@link JUnitResultFormatter} determines the test name by reflection.
+         */
+        public String getName() {
+            return description.getDisplayName();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            DescriptionAsTest that = (DescriptionAsTest) o;
+
+            if (!description.equals(that.description)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return description.hashCode();
+        }
+    }
+
+    /**
+     * Adopts {@link JUnitResultFormatter} into {@link RunListener},
+     * and also captures stdout/stderr by intercepting the likes of {@link System#out}.
+     *
+     * Because Ant JUnit formatter uses one stderr/stdout per one test suite,
+     * we capture each test case into a separate report file.
+     */
+    public static class JUnitResultFormatterAsRunListener extends RunListener {
+        protected final JUnitResultFormatter formatter;
+        private ByteArrayOutputStream stdout,stderr;
+        private PrintStream oldStdout,oldStderr;
+        private int problem;
+        private long startTime;
+
+        private JUnitResultFormatterAsRunListener(JUnitResultFormatter formatter) {
+            this.formatter = formatter;
+        }
+
+        @Override
+        public void testRunStarted(Description description) throws Exception {
+        }
+
+        @Override
+        public void testRunFinished(Result result) throws Exception {
+        }
+
+        @Override
+        public void testStarted(Description description) throws Exception {
+            formatter.startTestSuite(new JUnitTest(description.getDisplayName()));
+            formatter.startTest(new DescriptionAsTest(description));
+            problem = 0;
+            startTime = System.currentTimeMillis();
+
+            this.oldStdout = System.out;
+            this.oldStderr = System.err;
+            System.setOut(new PrintStream(stdout = new ByteArrayOutputStream()));
+            System.setErr(new PrintStream(stderr = new ByteArrayOutputStream()));
+        }
+
+        @Override
+        public void testFinished(Description description) throws Exception {
+            System.out.flush();
+            System.err.flush();
+            System.setOut(oldStdout);
+            System.setErr(oldStderr);
+
+            formatter.setSystemOutput(stdout.toString());
+            formatter.setSystemError(stderr.toString());
+            formatter.endTest(new DescriptionAsTest(description));
+
+            JUnitTest suite = new JUnitTest(description.getDisplayName());
+            suite.setCounts(1,problem,0);
+            suite.setRunTime(System.currentTimeMillis()-startTime);
+            formatter.endTestSuite(suite);
+        }
+
+        @Override
+        public void testFailure(Failure failure) throws Exception {
+            testAssumptionFailure(failure);
+        }
+
+        @Override
+        public void testAssumptionFailure(Failure failure) {
+            problem++;
+            formatter.addError(new DescriptionAsTest(failure.getDescription()), failure.getException());
+        }
+
+        @Override
+        public void testIgnored(Description description) throws Exception {
+            super.testIgnored(description);
+        }
+    }
+
+
 }
--- src/main/java/edu/cmu/cs/db/peloton/test/common/TestQuery.java
+++ src/main/java/edu/cmu/cs/db/peloton/test/common/TestQuery.java
@@ -1,15 +1,18 @@
 package edu.cmu.cs.db.peloton.test.common;
 
 import edu.cmu.cs.db.peloton.test.generate.ast.Context;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.runners.Parameterized.*;
 
 /**
@@ -20,7 +23,7 @@ public class TestQuery {
     @Parameters(name = "{index}: {0}")
     public static Iterable<?> data() {
         List<String> queries = new ArrayList<>();
-        DatabaseDefinition definition = Main.db.getDatabaseDefinition();
+        DatabaseDefinition definition = Main.testDb.getDatabaseDefinition();
         for (int i = 0; i < 20; i++) {
             queries.add(Main.tested.generate(definition, Context.EMPTY, Main.random).getClause());
         }
@@ -31,7 +34,29 @@ public class TestQuery {
 
     @Test
     public void test() throws SQLException {
-        System.out.println(query);
-        Main.db.getConnection().createStatement().execute(query);
+        testEqual(Main.testDb.getConnection().createStatement().executeQuery(query),
+                Main.oracleDb.getConnection().createStatement().executeQuery(query));
+    }
+
+    private static void testEqual(ResultSet truth, ResultSet target) throws SQLException {
+        Set<List<Object>> truthResults = new HashSet<>();
+        while (truth.next()) {
+            truthResults.add(getRow(truth, truth.getMetaData().getColumnCount()));
+        }
+
+        Set<List<Object>> targetResults = new HashSet<>();
+        while (target.next()) {
+            targetResults.remove(getRow(target, target.getMetaData().getColumnCount()));
+        }
+
+        assertEquals(truthResults, targetResults);
+    }
+
+    private static List<Object> getRow(ResultSet resultSet, int columnCount) throws SQLException {
+        List<Object> row = new ArrayList<>();
+        for (int i = 1; i <= columnCount; i++) {
+            row.add(resultSet.getObject(i));
+        }
+        return row;
     }
 }
