ad175a304a7f41d102e2f5cabf842235a15eaef8
Tianyu Li <tli2@andrew.cmu.edu>
2017-04-19 12:23:25 -0400

    demo

--- setup.sql
+++ setup.sql
@@ -1,16 +1,1 @@
-CREATE TABLE foo (
-    id INT,
-    value VARCHAR(255),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE bar (
-    id INT,
-    num INT,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO foo VALUES (42, 'foo');
-INSERT INTO foo VALUES (15, 'bar');
-INSERT INTO bar VALUES (10, 20);
-INSERT INTO bar VALUES (11, 25);
+INSERT INTO bar VALUES (12, 30);
